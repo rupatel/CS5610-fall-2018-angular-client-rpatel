80233d039c31da17b6c1025997a126c3f340456a
rupatel <patel.ru@husky.neu.edu>
2018-11-14 01:23:48 -0500

    injectable services boiler plate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,26 +3,37 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CourseGridComponent } from './course-grid-component.ts/course-grid-component.ts.component';
-import { ModuleListComponent } from './module-list-component.ts/module-list-component.ts.component';
-import { LessonTabsComponent } from './lesson-tabs-component.ts/lesson-tabs-component.ts.component';
-import { TopicPillsComponent } from './topic-pills-component.ts/topic-pills-component.ts.component';
-import { WidgetListComponent } from './widget-list-component.ts/widget-list-component.ts.component';
+import { TopicPillsComponentComponent } from './topic-pills-component/topic-pills-component.component';
+import { WidgetListComponentComponent } from './widget-list-component/widget-list-component.component';
+import { CourseGridComponentComponent } from './course-grid-component/course-grid-component.component';
+import { ModuleListComponentComponent } from './module-list-component/module-list-component.component';
+import { LessonTabsComponentComponent } from './lesson-tabs-component/lesson-tabs-component.component';
+import {CourseServiceClient} from './services/CourseServiceClient';
+import {LessonServiceClient} from './services/LessonServiceClient';
+import {ModuleServiceClient} from './services/ModuleServiceClient';
+import {TopicServiceClient} from './services/TopicServiceClient';
+import {WidgetServiceClient} from './services/WidgetServiceClient';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CourseGridComponent.TsComponent,
-    ModuleListComponent.TsComponent,
-    LessonTabsComponent.TsComponent,
-    TopicPillsComponent.TsComponent,
-    WidgetListComponent.TsComponent
+    TopicPillsComponentComponent,
+    WidgetListComponentComponent,
+    CourseGridComponentComponent,
+    ModuleListComponentComponent,
+    LessonTabsComponentComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    CourseServiceClient,
+    LessonServiceClient,
+    ModuleServiceClient,
+    TopicServiceClient,
+    WidgetServiceClient
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
