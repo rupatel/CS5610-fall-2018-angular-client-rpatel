5d9b2eddad228ff721b82656cafc12d1f852b585
rupatel <patel.ru@husky.neu.edu>
2018-11-17 01:32:13 -0500

    fetch modules using REST

--- src/app/module-list-component/module-list-component.component.ts
+++ src/app/module-list-component/module-list-component.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-module-list-component',
@@ -6,17 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./module-list-component.component.css']
 })
 export class ModuleListComponentComponent implements OnInit {
-  modules = [{id : 1, title : 'Mod1'},
-    {id : 2, title : 'Mod2'},
-    {id : 3, title : 'Mod3'},
-    {id : 4, title : 'Mod4'},
-    {id : 5, title : 'Mod5'},
-    {id : 6, title : 'Mod6'},
-    {id : 7, title : 'Mod7'},
-    {id : 8, title : 'Mod8'}];
+  @Input()
+  modules;
   constructor() { }
 
   ngOnInit() {
   }
-
 }
--- src/app/module/module.component.ts
+++ src/app/module/module.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-module',
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./module.component.css']
 })
 export class ModuleComponent implements OnInit {
-
+  @Input()
+  modules;
   constructor() { }
 
   ngOnInit() {
