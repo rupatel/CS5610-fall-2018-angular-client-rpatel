090817372c1e527b378e93bc7703c4ed6e8b5d52
rupatel <patel.ru@husky.neu.edu>
2018-11-17 23:41:20 -0500

    server config

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/a7submission'));
+app.use(express.static(__dirname + '/dist/CS5610AngularClient'));
 
 app.get('/*', function(req,res) {
 
-  res.sendFile(path.join(__dirname+'/dist/a7submission/index.html'));
+  res.sendFile(path.join(__dirname+'/dist/CS5610AngularClient/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
